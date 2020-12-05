91cd942975d83c41bcff0fa2d14c7ba9538bacc8
benny <benny08118@gmail.com>
2020-12-05 14:59:48 +0800

    add new;create card and edit card. view:show card

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -4,16 +4,41 @@ class CardsController < ApplicationController
     # render html: 'hello 123'
     #/app/views/cards/index.html.erb
     #erb=embedded ruby
+    @cards=Card.all
   end
 
   # Add a new card page
   def new
-
+    @card=Card.new
   end
 
   def create
+    clean_parims=params.require(:card).permit(:title,:content)
+    @card=Card.new(clean_parims)
+    if @card.save
+      redirect_to "/cards"
+    else
+      render :new
+    end
+  end
 
+ def edit
+
+   @card=Card.find(params[:id])
+ 
+ end
+
+ def update
+   @card=Card.find(params[:id])
+   clean_parims=params.require(:card).permit(:title,:content)
+ 
+   if @card.update(clean_parims)
     redirect_to "/cards"
+   else
+    render :edit
+   end
+
+
   end
 
 end
