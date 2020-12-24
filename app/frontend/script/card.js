180dcc0dac9d8e36d6c15e0cde2e0b77b1f2deda
benny <benny08118@gmail.com>
2020-12-24 15:50:16 +0800

    arrangement

--- app/frontend/script/card.js
+++ app/frontend/script/card.js
@@ -1,6 +1,6 @@
 document.addEventListener('turbolinks:load',()=>{
-      const btn=document.querySelector('.like_btn')
-      if(btn){
+    const btn=document.querySelector('.like_btn')
+    if(btn){
       btn.addEventListener("click",(e)=>{
       e.preventDefault()//暫停目前行為重新監聽事件
       // get page csrf token
@@ -17,6 +17,7 @@ document.addEventListener('turbolinks:load',()=>{
         {
           icon.classList.remove('far')
           icon.classList.add('fas')
+          console.log(icon)
         }else{
           icon.classList.remove('fas')
           icon.classList.add('far')
